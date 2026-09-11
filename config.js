e6f98d2223d9e0a89cacbb0ae7670a73bafb60ba
vt-store <victorpanis2@gmail.com>
2026-09-11 20:05:12 -0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ```javascript
 window.VT_CONFIG = {
-  SUPABASE_URL: "sb_publishable_s_OQXA8-YBgIBjOuRWkZMQ_B5QHKLef",
+  SUPABASE_URL: "sb_publishable_R8ESDaJFi_70YNtnki7_9w_1O0WxLT5",
 
   SUPABASE_ANON_KEY: "sb_publishable_R8ESDaJFi_70YNtnki7_9w_1O0WxLT5",
 
