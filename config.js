c7c9b7e67b3283ab02cc7dea884ad3a202e94e8a
vt-store <victorpanis2@gmail.com>
2026-09-11 20:20:55 -0300

    config.js
    window.VT_CONFIG = {
      SUPABASE_URL: "https://knmwxyxgcgytzhggzhhd.supabase.co",
      SUPABASE_ANON_KEY: "sb_publishable_R8ESDaJFi_70YNtnki7_9w_1O0WxLT5",
      WHATSAPP: "5515991713864",
      TIKTOK: "https://www.tiktok.com/@vt_store015"
    };

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-window.VT_CONFIG = {
-SUPABASE_URL: "https://knmwxyxgcgytzhggzhhd.supabase.co",
-SUPABASE_ANON_KEY: "sb_publishable_R8ESDaJFi_70YNtnki7_9w_1O0WxLT5",
-WHATSAPP: "5515991713864",
-TIKTOK: "https://www.tiktok.com/@vt_store015"
-};
+
